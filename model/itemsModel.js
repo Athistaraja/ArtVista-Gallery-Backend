10bcfd186545653c0419d3ba71840cd4b3ec50a5
Athistaraja <149251301+Athistaraja@users.noreply.github.com>
2024-06-17 21:42:03 +0530

    Update itemsModel.js

--- model/itemsModel.js
+++ model/itemsModel.js
@@ -6,6 +6,8 @@ const itemSchema = mongoose.Schema(
         price: { type: Number, required: true, },
         category: { type: String, required: true, },
         image: { type: String, required: true, },
+        artist:{ type: String, required: true, },
+        description:{ type: String, required: true, },
 
     },
     { timestamps: true }
