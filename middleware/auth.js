26b0d859077b304b896aae8d1e1d0ccf268ebb3d
Athistaraja <athisraja45@gmail.com>
2024-08-24 23:23:04 +0530

    Recent update

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,22 +2,19 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const auth = async (req, res, next) => {
-    try {
-      const token = req.header('x-auth-token').replace('Bearer ', '');
-      console.log('Received token:', token); // Debug log
-  
-      if (!token) {
+const auth = (req, res, next) => {
+    const token = req.header('x-auth-token')?.replace('Bearer ', '');
+    if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
     try {
+        
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded; // Assuming token contains user ID and role
         next();
     } catch (error) {
-      console.error('Token verification error:', error); // Debug log
-      res.status(401).json({ message: 'Token is not valid' });
+        res.status(401).json({ message: 'Token is not valid' });
     }
   };
  
